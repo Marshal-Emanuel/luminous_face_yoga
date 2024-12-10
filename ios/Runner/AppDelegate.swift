9b02bd9cdbc241b562957a192c0c0346fc0d7ffa
Emanuel Marshal <marshcodes@gmail.com>
2024-12-10 18:44:14 +0200

    [modified] appdelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import awesome_notifications
 import UserNotifications
-import flutter_inappwebview
+import AwesomeNotifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
